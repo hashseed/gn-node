e8b77afd0f00e4681ee89306bcc1a1ff62dce56b
Yang Guo <yangguo@chromium.org>
2019-01-28 13:36:11 +0100

    Generate config.gypi

--- tools/generate_config_gypi.py
+++ tools/generate_config_gypi.py
@@ -1,19 +1,29 @@
-# TODO: assess which if any of the config variables are important to include in
-# the js2c'd config.gypi.
+import re
+import subprocess
 import sys
 
-def main(args):
-  out = args[0]
-  with open(out, 'w') as f:
-      f.write("""
-{ 'target_defaults': { 'default_configuration' : 'Release' },
-  'variables': { 'v8_enable_inspector' : 1,
-                 'node_report' : 0,
-                 'node_shared_openssl' : 0,
-                 'v8_enable_i18n_support' : 1
-               }
-}
-""")
+GN_RE = re.compile(r'(\w+)\s+=\s+(.*?)$', re.MULTILINE)
+
+def get_template_content(template_file):
+  with open(template_file, "r") as f:
+    return f.read()
+
+def main(jinja_dir, gn_out_dir, template_file, output_file):
+  # Get GN config and parse into a dictionary.
+  gnconfig = subprocess.check_output(
+                 ["gn", "args", "--list", "--short", "-C", gn_out_dir])
+  config = dict(re.findall(GN_RE, gnconfig))
+
+  # Fill in template.
+  sys.path.append(jinja_dir)
+  from jinja2 import Template
+  template = Template(get_template_content(template_file))
+  rendered_template = template.render(config)
+
+  # Write output.
+  print(rendered_template)
+  with open(output_file, "w") as f:
+    f.write(rendered_template)
 
 if __name__ == '__main__':
-  main(sys.argv[1:])
+  main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
