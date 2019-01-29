c99e26de9f45bc776100748dbe7a87155ddebc56
Yang Guo <yangguo@chromium.org>
2019-01-29 11:56:17 +0100

    Add node_module_version variable

--- tools/generate_config_gypi.py
+++ tools/generate_config_gypi.py
@@ -1,6 +1,11 @@
 import re
+import os
 import subprocess
 import sys
+
+basedir = os.path.dirname(__file__)
+sys.path.append(os.path.join(basedir, os.pardir, "node", "tools"))
+import getmoduleversion
 
 GN_RE = re.compile(r'(\w+)\s+=\s+(.*?)$', re.MULTILINE)
 
@@ -13,6 +18,8 @@ def main(jinja_dir, gn_out_dir, template_file, output_file):
   gnconfig = subprocess.check_output(
                  ["gn", "args", "--list", "--short", "-C", gn_out_dir])
   config = dict(re.findall(GN_RE, gnconfig))
+
+  config['node_module_version'] = getmoduleversion.get_version()
 
   # Fill in template.
   sys.path.append(jinja_dir)
