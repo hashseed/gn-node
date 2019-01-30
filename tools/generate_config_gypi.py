60a0243c4712571755744d1ed02e301563d8bb6b
Yang Guo <yangguo@chromium.org>
2019-01-30 11:59:18 +0100

    Use number for process.config.variables.v8_enable_i18n_support

--- tools/generate_config_gypi.py
+++ tools/generate_config_gypi.py
@@ -9,7 +9,10 @@ import getmoduleversion
 
 GN_RE = re.compile(r'(\w+)\s+=\s+(.*?)$', re.MULTILINE)
 
-def get_template_content(template_file):
+def to_number_filter(v):
+  return 1 if v else 0
+
+def load_template(template_file):
   with open(template_file, "r") as f:
     return f.read()
 
@@ -19,12 +22,14 @@ def main(jinja_dir, gn_out_dir, template_file, output_file):
                  ["gn", "args", "--list", "--short", "-C", gn_out_dir])
   config = dict(re.findall(GN_RE, gnconfig))
 
-  config['node_module_version'] = getmoduleversion.get_version()
+  config["node_module_version"] = getmoduleversion.get_version()
 
   # Fill in template.
   sys.path.append(jinja_dir)
-  from jinja2 import Template
-  template = Template(get_template_content(template_file))
+  from jinja2 import Environment, FunctionLoader
+  env = Environment(loader=FunctionLoader(load_template))
+  env.filters["to_number"] = to_number_filter
+  template = env.get_template(template_file)
   rendered_template = template.render(config)
 
   # Write output.
