9067773b26cff84a84707256ad38ca5067ccde7d
Yang Guo <yangguo@chromium.org>
2019-02-06 21:56:45 +0100

    Use absolute path when executing node as build step
    This fixes the build if $PATH doesn't include `.`.
    
    R=machenbach@chromium.org
    
    Change-Id: I2195b91f820ddb4451cc5bbb323b0c2ea038c771
    Reviewed-on: https://chromium-review.googlesource.com/c/1456746
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>

--- tools/generate_code_cache.py
+++ tools/generate_code_cache.py
@@ -2,10 +2,12 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
 import subprocess
 import sys
 
 def main(node_exe, script, output):
+  node_exe = os.path.abspath(node_exe)
   subprocess.check_output(
       [node_exe, "--expose-internals", script, output])
 
