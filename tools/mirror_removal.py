9a1d3eadb7a22b58049576e22a1c9f836985e272
Yang Guo <yangguo@chromium.org>
2019-02-19 23:17:40 +0100

    Use shutil.rmtree to delete directories
    TBR=machenbach@chromium.org
    
    Change-Id: Ica7f577dd68c0837dd918ccb54f002a0a8427a2d
    Reviewed-on: https://chromium-review.googlesource.com/c/1478190
    Reviewed-by: Yang Guo <yangguo@chromium.org>

--- tools/mirror_removal.py
+++ tools/mirror_removal.py
@@ -6,6 +6,7 @@
 # in from_dir. This is to remove outdated artifacts in incremental builds.
 
 import os
+import shutil
 import sys
 
 def mirror_removal(from_dir, to_dir, dirpath, list, remove):
@@ -19,7 +20,7 @@ def main(from_dir, to_dir, stamp):
   os.chdir(to_dir)
   for dirpath, dirs, files in os.walk(".", topdown=False):
     mirror_removal(from_dir, to_dir, dirpath, files, lambda x: os.unlink(x))
-    mirror_removal(from_dir, to_dir, dirpath, dirs, lambda x: os.rmdir(x))
+    mirror_removal(from_dir, to_dir, dirpath, dirs, lambda x: shutil.rmtree(x))
 
   with open(stamp, 'w') as file:
     file.write('stamp')
